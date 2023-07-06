58a7f81d7570e8168de87d24a2bfafc4c9fba336
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-07-06 16:25:31 -0300

    first commit

---
+++ src/test/java/br/senai/lab365/semana10/Semana10ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.semana10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
